3d4066250eef2e04c64d08fe9e9e812722f70d7c
KellyPorter02 <porter.kelly02@gmail.com>
2021-03-25 06:20:32 -0400

    checkpointing

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import oracle.jrockit.jfr.StringConstantPool;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -9,6 +11,7 @@ public class StringArrayUtils {
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
+
         return array[0];
     }
 
@@ -17,6 +20,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
+
         return array[1];
     }
 
@@ -25,7 +29,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +38,8 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+
+        return array[array.length - 2];
     }
 
     /**
@@ -42,6 +48,10 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (String string : array) {
+            if (string.equals(value))
+                return true;
+        }
         return false;
     }
 
@@ -58,8 +68,13 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
+//        for (String string : array) {
+//            if (string.equals(value))
+//                return true;
+//        }
         return false;
     }
+
 
     /**
      * @param array array of String objects
@@ -69,7 +84,12 @@ public class StringArrayUtils {
         /* a pangramic sting is a sentence with all the letters in the alphabet
         in it.make a new array, if I find a, add to new array, if
          */
+//        for (String string : array) {
+//            if (string.equals(value))
+//                return true;
+
         return false;
+
     }
 
     /**
@@ -100,6 +120,15 @@ public class StringArrayUtils {
         add. at last element a[a.length-1], add? length of array is i = a.length,
         last element in array is a[last element] = a.length-1
          */
+//        int counter = 0;
+//        String[] removedDupes = new String[array.length];
+//        for (int i = 0; i < array.length - 1; i++) {
+//            if (array[i].equals(array[i + 1])) {
+//                removedDupes = array[i];
+//                i--;
+//            }
+
+        }
 
         return null;
     }
@@ -109,8 +138,15 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
+//        String[] newArr;
+//        if (int i = 0; i <= newArr.length - 1; i++) {
+//            if (array[i] == array[i + 1]) {
+//
+//            }
         return null;
     }
+
+
 
 
 }
